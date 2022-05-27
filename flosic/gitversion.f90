3c1df1db5a08e34ec49f74d15c985e5b79f07172
Yoh Yamamoto <yamamoto@localhost.localdomain>
2022-05-27 14:59:24 -0600

    Tested and edited LSIC/OSIC/sdSIC for qscf

--- flosic/gitversion.f90
+++ flosic/gitversion.f90
@@ -1,7 +1,7 @@
 SUBROUTINE GITVERSION
 
-PRINT '(A)','commit bcd6961794e0c7221d11216867d0a07a96c5fdbc'
+PRINT '(A)','commit 936ad2fc3d6c1a307f6cef00487553d91bcf4ba9'
 PRINT '(A)','Author: Yoh Yamamoto <yamamoto@localhost.localdomain>'
-PRINT '(A)','Date:   Wed May 25 21:33:37 2022 -0600'
+PRINT '(A)','Date:   Fri May 27 11:22:02 2022 -0600'
 
 END SUBROUTINE GITVERSION
