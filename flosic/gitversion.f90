6a2f310756b76a788b8bb1319695de3238e67f7b
Yoh Yamamoto <yamamoto@utep-psci118.utep.edu>
2022-06-14 13:49:41 -0600

    Consolidated scaled SIC calls

--- flosic/gitversion.f90
+++ flosic/gitversion.f90
@@ -1,7 +1,7 @@
 SUBROUTINE GITVERSION
 
-PRINT '(A)','commit 936ad2fc3d6c1a307f6cef00487553d91bcf4ba9'
-PRINT '(A)','Author: Yoh Yamamoto <yamamoto@localhost.localdomain>'
-PRINT '(A)','Date:   Fri May 27 11:22:02 2022 -0600'
+PRINT '(A)','commit 3fa93a6767aee6aabb7d3fbb425ff407e21d7cc6'
+PRINT '(A)','Author: Yoh Yamamoto <yohyamamoto@gmail.com>'
+PRINT '(A)','Date:   Mon May 30 14:16:59 2022 -0600'
 
 END SUBROUTINE GITVERSION
